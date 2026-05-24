b25224676415ae54fa7882434b3bc6bf6d0944fa
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-24 22:15:36 +0200

    [verde] - Implementado test en verde para eliminar productos de la lista

--- src/ListaDeLaCompra.php
+++ src/ListaDeLaCompra.php
@@ -8,11 +8,23 @@ class ListaDeLaCompra
 
     public function instruccion(string $instruccion): string{
         $partes = explode(" ", $instruccion);
+        $accion = $partes[0];
         $nombre = strtolower($partes[1]);
-        $cantidad = $partes[2];
-        if ($cantidad == null){
-            $cantidad = 1;
+
+
+        if($accion === "añadir"){
+            $cantidad = isset($partes[2]) ? (int)$partes[2] : 1;
+            $this->listaDeLaCompra[$nombre] = $cantidad;
         }
-        return "$nombre x$cantidad";
+        if($accion === "eliminar"){
+            unset($this->listaDeLaCompra[$nombre]);
+        }
+
+
+        return implode(", ", array_map(
+            fn($n, $c) => "$n x$c",
+            array_keys($this->listaDeLaCompra),
+            $this->listaDeLaCompra
+        ));
     }
 }
