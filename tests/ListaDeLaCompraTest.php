4bffc09db70cfd230891d8d70ba0969fc966d5e4
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-24 12:12:41 +0200

    [rojo] - Creados tests en rojo para añadir productos

---
+++ tests/ListaDeLaCompraTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Deg540\CleanCodeKata9\Test;
+
+use Deg540\CleanCodeKata9\ListaDeLaCompra;
+use PHPUnit\Framework\TestCase;
+
+class ListaDeLaCompraTest extends TestCase
+{
+
+    public function testCarrito()
+    {
+
+    }
+}
